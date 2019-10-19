aa66fa8b6814506270112e0e6efe339e5f334a9d
feat7 <vinaykhobragade99@gmail.com>
2019-10-19 23:43:28 +0530

    deploy: ONNX car model

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, request, jsonify
 from convert.keras import KerasONNX
+import onnxruntime as rt
+import numpy as np
+from PIL import Image
+import requests
 
 app = Flask(__name__)
 
@@ -26,6 +30,51 @@ def keras_to_onnx(project_id):
 
     return jsonify(success=success)
 
+
+@app.route('/inference/<project_id>', methods=['POST'])
+def image_classification_inference(project_id):
+    # TODO: Get model information from project id @onnx_model
+    # ONNX Model
+    model_path = request.get_json()['model_path']
+    image_url = request.get_json()['image_url']
+
+    sess = rt.InferenceSession(model_path)
+    input_name = sess.get_inputs()[0].name
+    label_name = sess.get_outputs()[0].name
+    expected_shape = sess.get_inputs()[0].shape
+
+    response = requests.get(image_url, stream=True)
+    response.raw.decode_content = True
+    image = preprocess(response.raw, shape=(100, 100, 1), grayscale=True, normalize=True)
+
+    print(image.shape)
+
+    result = sess.run([], {input_name: image})
+
+    # TODO: Remap the classes
+    classes = ["car", "not a car"]
+    prob = result[0][0]
+    
+    return jsonify(success=True, result=classes[np.argmax(prob)], confidence=str(prob.max()))
+
+
+def preprocess(image_url, shape=(224, 224, 3), grayscale=False, normalize=True):
+    image = Image.open(image_url)
+    if (grayscale):
+        image = image.convert('L')
+
+    print(shape)
+    image = image.resize(shape[:-1])
+    image = np.array(image)
+
+    if (normalize):
+        image = image.astype(np.float32) / 255
+
+    image = np.array([image])
+    image = np.array([image]).reshape(shape)
+    image = image[np.newaxis]
+
+    return image
 
 if __name__ == '__main__':
     app.run(debug=True)
