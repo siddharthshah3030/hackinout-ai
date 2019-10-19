aed1f089dc5832235208a47e28e07821f1435a8d
feat7 <vinaykhobragade99@gmail.com>
2019-10-19 21:34:37 +0530

    add: API keras to ONNX conversion

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from convert.keras import KerasONNX
 
 app = Flask(__name__)
@@ -8,12 +8,24 @@ def hello_world():
     return 'Hello, World!'
 
 
-def keras_to_onnx(model_path, save_path):
-    keras_onnx = KerasONNX(model_path, save_path)
-    keras_onnx.convert()
-    keras_onnx.save()
+@app.route('/convert/<project_id>', methods = ['POST'])
+def keras_to_onnx(project_id):
+    # TODO: Get model information from project id @model_path and @save_path
+    model_path = request.get_json()['model_path']
+    save_path = request.get_json()['save_path']
+
+    success = False 
+
+    try:
+        keras_onnx = KerasONNX(model_path, save_path)
+        keras_onnx.convert()
+        keras_onnx.save()
+        success = True
+    except:
+        success = False
+
+    return jsonify(success=success)
 
 
 if __name__ == '__main__':
-    keras_to_onnx('mobilenet.h5', 'mobilenet.onnx')
     app.run(debug=True)
--- convert/keras.py
+++ convert/keras.py
@@ -1,20 +1,27 @@
 import onnxmltools
-import tensorflow as tf
 from keras.models import load_model
-from keras import backend as K
+import keras2onnx
 
 
 class KerasONNX():
     def __init__(self, model_path, onnx_model_path):
-        K.clear_session()
-        self.model = tf.reset_default_graph()
-
         self.model_path = model_path
         self.onnx_model_path = onnx_model_path
         self.model = load_model(model_path)
 
     def convert(self):
-        self.onnx = onnxmltools.convert_keras(self.model)
+        try:
+            self.onnx = onnxmltools.convert_keras(self.model)
+            return True
+        except:
+            return False
+            
 
     def save(self):
-        onnxmltools.utils.save_model(self.onnx, self.onnx_model_path)
+        try:
+            onnxmltools.utils.save_model(self.onnx, self.onnx_model_path)
+            return True
+        except:
+            return False
+
+            
