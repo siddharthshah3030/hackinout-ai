9178a8d21f20b65924e68b9566d7f79a0610d976
feat7 <vinaykhobragade99@gmail.com>
2019-10-19 20:58:27 +0530

    onnx car model working

--- app.py
+++ app.py
@@ -8,5 +8,12 @@ def hello_world():
     return 'Hello, World!'
 
 
+def keras_to_onnx(model_path, save_path):
+    keras_onnx = KerasONNX(model_path, save_path)
+    keras_onnx.convert()
+    keras_onnx.save()
+
+
 if __name__ == '__main__':
+    keras_to_onnx('mobilenet.h5', 'mobilenet.onnx')
     app.run(debug=True)
--- convert/keras.py
+++ convert/keras.py
@@ -1,8 +1,14 @@
 import onnxmltools
+import tensorflow as tf
 from keras.models import load_model
+from keras import backend as K
+
 
 class KerasONNX():
     def __init__(self, model_path, onnx_model_path):
+        K.clear_session()
+        self.model = tf.reset_default_graph()
+
         self.model_path = model_path
         self.onnx_model_path = onnx_model_path
         self.model = load_model(model_path)
